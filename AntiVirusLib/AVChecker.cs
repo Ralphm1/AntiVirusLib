3d2671f47226ed5ff7ba34888007427960b411a3
Ralph Mace <ralphm1@cox.net>
2018-04-23 16:36:55 -0400

    Add project files.

---
+++ AntiVirusLib/AVChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AntiVirus;
+
+namespace AntiVirusLib
+{
+    public class AVChecker
+    {
+        public String ScanFile(String filePath)
+        {
+            Scanner scanner = new Scanner();
+            String result = scanner.ScanAndClean(filePath).ToString();
+
+            return result;
+        }
+
+        public ScanResult ScanFileEnum(String filePath)
+        {
+            Scanner scanner = new Scanner();
+            ScanResult result = scanner.ScanAndClean(filePath);
+
+            return result;
+        }
+
+    }
+}
